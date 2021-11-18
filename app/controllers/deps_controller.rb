f038937725b9a60d02c28b661857ffe100393b5b
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-11-17 21:40:22 -0700

    filled out deps controller, ready to do js now

--- app/controllers/deps_controller.rb
+++ app/controllers/deps_controller.rb
@@ -1,5 +1,8 @@
 class DepsController < ApplicationController
 
+    before_action :set_dep, only:[:show, :update, :edit, :destroy]
+    # make sure to get the current department before executing these actions
+
     def index
         # Will show all departments and allow you to make new departments, view individual departments,
         # edit departments, and delete departments
@@ -12,21 +15,48 @@ class DepsController < ApplicationController
     end
 
     def show
+        render component: "Dep", props: { dep: @dep }
+        # this will go to my js that will display all department info and links
+        ## need to do
     end
     
     def new
+        render component: "DepsNew"
+        # this will bring up a form to input new department 
+        ## need to do
     end
 
     def edit
+        render component: "DepEdit", props: { dep: @dep }
+        # this component will bring up a form for changing the selected department
+        ## need to do
     end
 
     def create
+        # once the form is filled out you get the params and set them to a new department
+        Dep.create(name: params[:dep][:name], aisle_no: params[:dep][:aisle_no])
+        # I want to return to the home page to see the new department in my list of all of them
+        redirect_to deps_path
     end
 
     def update
+        @dep.update(name: params[:dep][:name], aisle_no: params[:dep][:aisle_no])
+        # this will change the selected dep with the input params from the form in edit
+        redirect_to dep_path
+        # I want to redirect back to that user to view my changes
     end
 
     def destroy
+        @dep.destroy
+        redirect_to deps_path
+        # actually destroy the input user and send you back to the home
+    end
+
+    private
+    def set_dep
+        @dep = Dep.find(params[:id])
+        # any function that needs to use the specific user (aka show, update, edit, and destroy)
+        # can call this instance variable
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "deps#index"
+  # re rout the root (get "/" to:) to the index of my departments since that will
+  # basically be my home page
   resources :deps
   # link the CRUD routes for the departments controller
 end
